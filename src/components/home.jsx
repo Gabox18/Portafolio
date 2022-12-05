ae7b5984c10e7116b5a3da64a06627c8c3ffab3c
Gabriel Ferrer <gefv1891@gmail.com>
2022-12-05 01:23:18 -0400

    menu en mobile y layaut en escritorio

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,30 +1,43 @@
-import { Grid, GridItem, useMediaQuery } from '@chakra-ui/react'
+import { Box, Button, Grid, GridItem, Heading, IconButton, Image, Stack } from '@chakra-ui/react'
+import NavMovile from './navMovile'
+import profile from '../assets/profilePhoto.jpg'
+import { ArrowForwardIcon, EmailIcon, HamburgerIcon } from '@chakra-ui/icons'
 
 function Home() {
-  const [isLargerThanHD] = useMediaQuery([
-    '(min-width: 720px)',
-    '(display-mode: browser)',
-  ])
+
+
   return (
-    <Grid
-      h='100vh'
-      templateRows='repeat(6, 1fr)'
-      templateColumns='repeat(5, 1fr)'
-      gap={1}
-    >
-      <GridItem rowSpan={2} colSpan={1} bg='tomato' minW='200px'>
-        area de foto
-      </GridItem>
-      <GridItem colSpan={4} rowSpan={6}
-      bg='papayawhip' >
-        main<br></br>
-        {isLargerThanHD?'holla':'hello'}
-      </GridItem>
-      <GridItem bg='purple'
-      rowSpan={4} >
-        menu
-      </GridItem>
-    </Grid>
+    <Box>
+      <Box display={{ base: 'block', md: 'none' }}>
+        <NavMovile />
+      </Box>
+
+      <Grid
+        h='100vh'
+        templateRows='repeat(6, 1fr)'
+        templateColumns='repeat(5, 1fr)'
+      >
+        <GridItem rowSpan={3} colSpan={1} bg='brand.primary' minW='200px' display={{ base: 'none', md: 'block' }} p={'2px'}>
+          <Image objectFit='cover' src={profile} alt={'gabriel ferrer'} boxSize='100%' />
+        </GridItem>
+
+        <GridItem colSpan={{ base: 6, md: 4 }} rowSpan={6} bg='primary'>
+          main
+        </GridItem>
+
+        <GridItem bg='brand.primary' rowSpan={4} display={{ base: 'none', md: 'block' }} >
+          <Stack direction='column'>
+            <Heading size={'md'}>Gabriel Ferrer</Heading>
+            <Button leftIcon={<EmailIcon />} colorScheme='teal' variant='solid'>
+              Home
+            </Button>
+            <Button rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline'>
+              Sobre Mi
+            </Button>
+          </Stack>
+        </GridItem>
+      </Grid>
+    </Box>
   )
 }
 
---
+++ src/components/navMovile.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import {
+    Drawer,
+    DrawerOverlay,
+    DrawerContent,
+    useDisclosure,
+    Box,
+    Flex,
+    Heading,
+    IconButton,
+    Image
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import profile from '../assets/profilePhoto.jpg'
+
+function NavMovile() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
+
+    return (
+        <>
+            <Flex minWidth='max-content' alignItems='center'>
+                <Box p='2' bg={'brand.primary'} width={'100%'}>
+                    <Heading size='md' color={'brand.700'}>Gabriel Ferrer</Heading>
+                </Box>
+                <IconButton ref={btnRef} bg={'brand.primary'} color={'brand.700'} onClick={onOpen} borderRadius={'0'}
+                    icon={<HamburgerIcon />}>
+                    Open
+                </IconButton>
+            </Flex>
+
+
+
+            <Drawer
+                isOpen={isOpen}
+                placement='left'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+                size={'xs'}
+            >
+                <DrawerOverlay />
+                <DrawerContent>
+                    <Image src={profile} alt={'gabriel ferrer'} />
+                </DrawerContent>
+            </Drawer>
+        </>
+    )
+}
+
+export default NavMovile
