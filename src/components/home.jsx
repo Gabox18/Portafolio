a8aa215ff29c6830a37fb272ca37a2f96f977600
Gabriel Ferrer <gefv1891@gmail.com>
2022-12-13 00:34:13 -0400

    blur y overlay en img de fondo

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Grid, GridItem, Image, Stack } from '@chakra-ui/react'
+import { Box, Button, Divider, Flex, Grid, GridItem, Image, Stack, Link, Text } from '@chakra-ui/react'
 import NavMovile from './navMovile'
 import profile from '../assets/profilePhoto.webp'
 import Bg from '../assets/fondo-home.webp'
@@ -23,45 +23,57 @@ function Home() {
         templateColumns='repeat(6, 1fr)'
       >
         <GridItem rowSpan={2} colSpan={1} bg='brand.primary' display={{ base: 'none', md: 'block' }} p={'2px'}>
-          <Image objectFit='contain' src={profile} alt={'gabriel ferrer'} />
+          <Image objectFit='cover' h={'100%'} w={'100%'} src={profile} alt={'gabriel ferrer'} />
         </GridItem>
 
-        <GridItem colSpan={{ base: 6, md: 5 }} rowSpan={6} backgroundColor={'rgb(0 0 0 / 80%)'}>
-          <Image objectFit='cover' src={Bg} alt={'fondo Home'} h={'100vh'} w={'100%'} />
+        <GridItem colSpan={{ base: 6, md: 5 }} rowSpan={6} >
+          <Box h={'100vh'} bgImage={`url(${Bg})`} bgRepeat={'no-repeat'} bgSize={'cover'} filter={'blur(1.2px)'}>
+            <Flex bg={'rgb(0 0 0 / 10%)'} h={'100vh'} />
+            {/* <Image objectFit='cover' src={Bg} alt={'fondo Home'} h={'100vh'} w={'100%'} /> */}
+          </Box>
         </GridItem>
 
         <GridItem bg='brand.primary' rowSpan={4} display={{ base: 'none', md: 'block' }} >
           <Stack direction='column'>
             {/* <Heading size={'sm'} marginBottom={'5px'} color={'white'}>Gabriel Ferrer</Heading> */}
             <Divider />
-            <Button leftIcon={<AiOutlineHome size={'20px'} />} colorScheme='teal' variant='ghost' borderRadius={0} h={'30px'}>
-              Home
+            <Button leftIcon={<AiOutlineHome size={'20px'} />} colorScheme='teal' variant='ghost' borderRadius={0} h={'30px'} iconSpacing={'5px'}>
+              Home        
             </Button>
             <Divider />
-            <Button leftIcon={<AiOutlineUser size={'20px'} />} colorScheme='teal' variant='ghost' borderRadius={0} h={'30px'}>
-              Sobre Mi
+            <Button leftIcon={<AiOutlineUser size={'20px'} />} colorScheme='teal' variant='ghost' borderRadius={0} h={'30px'} iconSpacing={'5px'}>
+              Sobre Mi  
             </Button>
             <Divider />
-            <Button leftIcon={<AiOutlinePicRight size={'20px'} />} colorScheme='teal' variant='ghost' borderRadius={0} h={'30px'}>
-              Resumen
+            <Button leftIcon={<AiOutlinePicRight size={'20px'} />} colorScheme='teal' variant='ghost' borderRadius={0} h={'30px'} iconSpacing={'5px'}>
+              Resumen   
             </Button>
             <Divider />
-            <Button leftIcon={<BsBriefcase size={'20px'} />} colorScheme='teal' variant='ghost' borderRadius={0} h={'30px'}>
+            <Button leftIcon={<BsBriefcase size={'20px'} />} colorScheme='teal' variant='ghost' borderRadius={0} h={'30px'} iconSpacing={'5px'}>
               Portafolio
             </Button>
             <Divider />
-            <Button leftIcon={<AiOutlineMail size={'20px'} />} colorScheme='teal' variant='ghost' borderRadius={0} h={'30px'}>
-              Contacto
+            <Button leftIcon={<AiOutlineMail size={'20px'} />} colorScheme='teal' variant='ghost' borderRadius={0} h={'30px'} iconSpacing={'5px'}>
+              Contacto  
             </Button>
             <Divider />
           </Stack>
-            <Stack direction='row' justify={'center'} marginTop={'6rem'}>
+          <Stack direction='row' justify={'center'} marginTop={'6rem'}>
+            <Link href='https://www.linkedin.com/in/gabriel-ferrer-villasmil/' isExternal>
               <FaLinkedin size={'1.5rem'} color='teal' />
+            </Link>
+            <Link href='https://github.com/Gabox18' isExternal>
               <FaGithubSquare size={'1.5rem'} color='teal' />
-              <ImMail size={'1.5rem'} color='teal' />
-            </Stack>
-            {`2022 © Gabriel Ferrer.\n
-              All Right Reserved.`} 
+            </Link>
+            <Link href='mailto:gefv191@gmail.com?Subject=Buenas%20te%20Contacto%20mediante%20tu%20Portafolio'>
+              <ImMail size={'1.43rem'} color='teal' />
+            </Link>
+          </Stack>
+          <Text fontSize={'9pt'} textAlign={'center'} color={'gray'} marginTop={'15px'}>
+            2022 &copy; Gabriel Ferrer
+            <br />
+            All Right Reserved.
+          </Text>
         </GridItem>
       </Grid>
     </Box>
