7eaafbf06ccb6241e0685541f4d9dcd54adf30e0
Gabriel Ferrer <gefv1891@gmail.com>
2022-12-26 23:19:26 -0400

    estilo al nav movile

--- src/components/navMovile.jsx
+++ src/components/navMovile.jsx
@@ -8,10 +8,19 @@ import {
     Flex,
     Heading,
     IconButton,
-    Image
+    Image,
+    Divider,
+    Button,
+    Stack,
+    Link,
+    Text
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import profile from '../assets/profilePhoto.webp'
+import { AiOutlineHome, AiOutlineMail, AiOutlinePicRight, AiOutlineUser } from 'react-icons/ai'
+import { BsBriefcase } from 'react-icons/bs'
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa'
+import { ImMail } from 'react-icons/im'
 
 function NavMovile() {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -19,9 +28,9 @@ function NavMovile() {
 
     return (
         <>
-            <Flex minWidth='max-content' alignItems='center'>
-                <Box p='2' bg={'brand.primary'} width={'90%'}>
-                    <Heading size='md' color={'brand.700'}>Gabriel Ferrer</Heading>
+            <Flex minWidth='max-content' alignItems='center' bg={'brand.primary'}>
+                <Box width={'90%'}>
+                    <Heading size='md' color={'brand.700'} height={'100%'} marginLeft={'12%'}>Gabriel Ferrer</Heading>
                 </Box>
                 <IconButton ref={btnRef} bg={'brand.primary'} color={'brand.700'} onClick={onOpen} borderRadius={'0'}
                     icon={<HamburgerIcon padding={'0'} />}>
@@ -37,8 +46,86 @@ function NavMovile() {
                 size={'xs'}
             >
                 <DrawerOverlay />
-                <DrawerContent>
-                    <Image src={profile} alt={'gabriel ferrer'} width={'12rem'} margin={'0 auto'}/>
+                <DrawerContent bg={'brand.primary'}>
+                    <Heading size='lg' color={'teal'} textAlign={'center'} paddingTop={'0.8rem'}>Gabriel Ferrer</Heading>
+                    <Image src={profile} alt={'gabriel ferrer'} width={'12rem'} margin={'2rem auto'} borderRadius={'full'}
+                        marginTop={'1rem'}/>
+                    <Divider marginTop={'1rem'} w={'80%'} margin={'0 auto'}/>
+                    <Button leftIcon={<AiOutlineHome size={'1.7rem'} />}
+                        color='teal'
+                        variant='ghost'
+                        borderRadius={0}
+                        h={'30px'}
+                        p={'1.5rem'}
+                        iconSpacing={'5px'}
+                        fontSize={'1.5rem'}>
+                        Home
+                    </Button>
+
+                    <Divider w={'80%'} margin={'0 auto'}/>
+                    <Button leftIcon={<AiOutlineUser size={'1.7rem'} />}
+                        color='teal'
+                        variant='ghost'
+                        borderRadius={0}
+                        h={'30px'}
+                        p={'1.5rem'}
+                        iconSpacing={'5px'}
+                        fontSize={'1.5rem'}>
+                        Sobre Mi
+                    </Button>
+
+                    <Divider w={'80%'} margin={'0 auto'}/>
+                    <Button leftIcon={<AiOutlinePicRight size={'1.7rem'} />}
+                        color='teal'
+                        variant='ghost'
+                        borderRadius={0}
+                        h={'30px'}
+                        p={'1.5rem'}
+                        iconSpacing={'5px'}
+                        fontSize={'1.5rem'}>
+                        Resumen
+                    </Button>
+
+                    <Divider w={'80%'} margin={'0 auto'}/>
+                    <Button leftIcon={<BsBriefcase size={'1.7rem'} />}
+                        color='teal'
+                        variant='ghost'
+                        borderRadius={0}
+                        h={'30px'}
+                        p={'1.5rem'}
+                        iconSpacing={'5px'}
+                        fontSize={'1.5rem'}>
+                        Portafolio
+                    </Button>
+
+                    <Divider w={'80%'} margin={'0 auto'}/>
+                    <Button leftIcon={<AiOutlineMail size={'1.7rem'} />}
+                        color='teal'
+                        variant='ghost'
+                        borderRadius={0}
+                        h={'30px'}
+                        p={'1.5rem'}
+                        iconSpacing={'5px'}
+                        fontSize={'1.5rem'}>
+                        Contacto
+                    </Button>
+                    <Divider w={'80%'} margin={'0 auto'}/>
+                    <Stack direction='row' justify={'center'} marginTop={'6rem'} spacing={'1.5rem'}>
+                        <Link href='https://www.linkedin.com/in/gabriel-ferrer-villasmil/' isExternal>
+                            <FaLinkedin size={'2rem'} color='teal' />
+                        </Link>
+                        <Link href='https://github.com/Gabox18' isExternal>
+                            <FaGithubSquare size={'2rem'} color='teal' />
+                        </Link>
+                        <Link href='mailto:gefv191@gmail.com?Subject=Buenas%20te%20Contacto%20mediante%20tu%20Portafolio'>
+                            <ImMail size={'1.97rem'} color='teal' />
+                        </Link>
+                    </Stack>
+                    <Text fontSize={'9.5pt'} textAlign={'center'} color={'gray'} marginTop={'15px'}>
+                        2022 &copy; Gabriel Ferrer
+                        <br />
+                        All Right Reserved.
+                    </Text>
                 </DrawerContent>
             </Drawer>
         </>
